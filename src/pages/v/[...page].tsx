872f780048862a71d47ff53ecc7149341f74dd7d
imnyang <nyang.nyanglab@gmail.com>
2023-09-19 21:41:24 +0900

    Update [...page].tsx

--- src/pages/v/[...page].tsx
+++ src/pages/v/[...page].tsx
@@ -6,13 +6,12 @@ import ReactMarkdown from 'react-markdown';
 
 export default function Page() {
   const router = useRouter();
-  useEffect(() => {
-    if (router.isReady) {
-      // Code using query
-      console.log(router.query.page[0]);
-    }
-  }, [router.isReady]);
+  const queries = router.query;
 
+  useEffect(() => {
+    if (!router.isReady) return;
+    console.log(queries.page, '🙆‍♀️ 콘솔에 쿼리 찍힘!')
+  }, [router.isReady])
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [date, setDate] = useState('');
@@ -21,7 +20,7 @@ export default function Page() {
   useEffect(() => {
     // 여기서 데이터를 가져오는 API 요청을 수행하고 값을 설정합니다.
     // 예를 들어, /api/items/slub 경로로 API 요청을 보내고 결과를 받아온다고 가정합니다.
-    fetch(`/api/items/${router.query.page[0]}`)
+    fetch(`/api/items/${router.query.page}`)
       .then(response => response.json())
       .then(data => {
         // API에서 받아온 데이터를 사용해 값을 설정합니다.
