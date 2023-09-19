b12e291967a90968be9732c5cffecddef67dbc32
imnyang <nyang.nyanglab@gmail.com>
2023-09-19 22:11:41 +0900

    어우 하드하드코딩

--- src/pages/v/[...page].tsx
+++ src/pages/v/[...page].tsx
@@ -14,12 +14,15 @@ export default function Page() {
     console.log(pageParam, 'Let\'s Go!');
   }, [router.isReady]);
 
+  const [loading, setLoading] = useState(true); // 로딩 상태 추가
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [date, setDate] = useState('');
   const [time, setTime] = useState('');
 
   useEffect(() => {
+    setLoading(true); // API 요청 전에 로딩 상태를 true로 설정
+
     // 여기서 데이터를 가져오는 API 요청을 수행하고 값을 설정합니다.
     // 예를 들어, /api/items/slub 경로로 API 요청을 보내고 결과를 받아온다고 가정합니다.
     fetch(`/api/items/${pageParam}`)
@@ -30,9 +33,12 @@ export default function Page() {
         setDescription(data.description);
         setDate(data.date);
         setTime(data.time);
+        setLoading(false); // API 요청 완료 후 로딩 상태를 false로 설정
+        console.log(data.description);
       })
       .catch(error => {
         console.error('API 요청 실패:', error);
+        setLoading(false); // API 요청 실패 시에도 로딩 상태를 false로 설정
       });
   }, [pageParam]);
 
@@ -49,9 +55,15 @@ export default function Page() {
         </div>
         <br /><br />
         <h1 className="ml-6">{title}</h1>
-        <div className="markdown">
-          <ReactMarkdown>{description}</ReactMarkdown>
-        </div>
+        {loading ? (
+          // 로딩 중일 때 로딩 화면을 표시
+          <p>로드중...</p>
+        ) : (
+          // 로딩이 완료되면 Markdown 내용을 표시
+          <div className="markdown">
+            <ReactMarkdown>{description}</ReactMarkdown>
+          </div>
+        )}
         <br />
         <p className="text-gray-400">{`Date: ${date} - Time: ${time} · Post in localhost`}</p>
       </div>
